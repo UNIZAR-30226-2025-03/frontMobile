c68d0c5548af5fc660ebfce24178264c188327a6
Solty19 <149105292+Solty19@users.noreply.github.com>
2025-03-19 21:44:24 +0100

    Cambio a RN-SingIn

--- EchoBeat/screens/Login_Register.js
+++ EchoBeat/screens/Login_Register.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView, Image, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as Google from 'expo-auth-session/providers/google';
-import * as WebBrowser from 'expo-web-browser';
-import Constants from 'expo-constants';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 
-WebBrowser.maybeCompleteAuthSession();
+GoogleSignin.configure({
+  androidClientId: '726836881808-97h90fu5165ajmpp1a81ip1pdfs23cij.apps.googleusercontent.com',
+  offlineAccess: false, // Si necesitas un refresh token, cámbialo a true
+});
 
 export default function Login_Register({ navigation }) {
   const [email, setEmail] = useState('');
@@ -13,25 +14,10 @@ export default function Login_Register({ navigation }) {
   const [errorMessage, setErrorMessage] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const isProduction = !Constants.expoConfig?.extra?.debug; // Usa Constants.expoConfig
-
-  const redirectUri = isProduction
-    ? 'echobeat://oauth2redirect' // URI de redirección en producción
-    : 'https://auth.expo.io/@rsoler19/EchoBeat'; // URI de redirección en desarrollo
-
-  console.log("URI de redirección:", redirectUri); // Verifica la URI que se está utilizando
-
-  const config = {
-    androidClientId: '726836881808-97h90fu5165ajmpp1a81ip1pdfs23cij.apps.googleusercontent.com',
-    responseType: 'code',
-    redirectUri, // Usa la URI de redirección correcta según el entorno
-  };
-
   // Configuración de Google Login
-  const [request, response, promptAsync] = Google.useAuthRequest(config);
 
   useEffect(() => {
-    console.log(response);
+    /*console.log(response);
     if (response?.type === 'success') {
       const { code } = response.params;
       if (code) {
@@ -39,8 +25,8 @@ export default function Login_Register({ navigation }) {
       } else {
         Alert.alert("Error", "No se pudo obtener el code de Google.");
       }
-    }
-  }, [response]);
+    }*/
+  }, []);
 
   const handleLogin = async () => {
     try {
@@ -64,32 +50,34 @@ export default function Login_Register({ navigation }) {
     }
   };
 
-  const handleGoogleLogin = async () => {
-    await promptAsync(); // Abre el navegador para iniciar sesión con Google
-  };
-
-  const loginWithGoogle = async (code) => {
-    try {
-      const response = await fetch("https://echobeatapi.duckdns.org/auth/google/code", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ code }),
-      });
-      console.log("Codigo mandado: ", code);
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error(data.message || "No autorizado: El usuario no tiene cuenta registrada");
-      }
-      if (data.accessToken) {
-        console.log("Token: ", data.accessToken);
-        await AsyncStorage.setItem("token", data.accessToken);
-        await AsyncStorage.setItem("email", data.user.email);
-        navigation.replace("Welcome");
-      }
-    } catch (error) {
-      Alert.alert("Error", error.message);
+  const loginWithGoogle = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const response = await GoogleSignin.signIn();
+    console.log(response);
+    if (isSuccessResponse(response)) {
+      setState({ userInfo: response.data });
+    } else {
+      // sign in was cancelled by user
     }
-  };
+  } catch (error) {
+    if (isErrorWithCode(error)) {
+      switch (error.code) {
+        case statusCodes.IN_PROGRESS:
+          // operation (eg. sign in) already in progress
+          break;
+        case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+          // Android only, play services not available or outdated
+          setErrorMessage("Google Play Services no está disponible o está desactualizado.");
+          break;
+        default:
+        // some other error happened
+      }
+    } else {
+      // an error that's not related to google sign in occurred
+    }
+  }
+};
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -141,8 +129,7 @@ export default function Login_Register({ navigation }) {
         {/* Botón de Iniciar con Google */}
         <TouchableOpacity 
           style={styles.googleButton} 
-          onPress={handleGoogleLogin}
-          disabled={!request}
+          onPress={loginWithGoogle}
         >
           <Text style={styles.googleButtonText}>INICIAR CON </Text>
           <Image 
